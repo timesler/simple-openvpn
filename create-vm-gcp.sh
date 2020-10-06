b4fc943a765c24fe81481e5864365f78f18d36df
Tim Esler <tim.esler@gmail.com>
2020-10-05 21:34:06 -0700

    Allow udp 1194

--- create-vm-gcp.sh
+++ create-vm-gcp.sh
@@ -4,7 +4,7 @@ gcloud beta compute --project=playground-timesler instances create simple-openvp
     --zone=us-west1-a \
     --machine-type=f1-micro \
     --can-ip-forward \
-    --tags=http-server,https-server \
+    --tags=http-server,https-server,openvpn-server \
     --image=cos-77-12371-1079-0 \
     --image-project=cos-cloud
 
